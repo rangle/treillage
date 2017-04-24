5899ae6f1e0a042efed53863845a9963d2844748
Roberto von Schoettler <roberto.vonschoettler@gmail.com>
2017-04-24 15:44:33 -0400

    Adding no empty body rule, supporting arrays of error messages

--- src/services/rules.js
+++ src/services/rules.js
@@ -3,16 +3,20 @@ const maxSize = 100;
 
 const errors = {
   maxLength: `This card is over ${maxSize} characters long, please shorten.`,
+  noEmptyBody: 'This card\'s description is empty.',
 };
+
+const noEmptyBody = (card) => card.desc.length === 0 && errors.noEmptyBody;
 
 const maxLength = (card) => {
   const size = (card.name + ' ' + card.desc).split(' ').length;
 
-  return size < maxSize ? ok : errors.maxLength;
+  return size > maxSize && errors.maxLength;
 };
 
 export const PASS = ok;
 
 export default [
+  noEmptyBody,
   maxLength,
 ];
