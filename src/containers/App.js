6208958e949c64cbb838bd13b1799e0d529c4fe4
Roberto von Schoettler <schoettler@users.noreply.github.com>
2017-04-25 16:05:19 -0400

    Changed action button to Approve. Bonus new nav menu. (#16)
    * Adding filtering by mention. Adding bluebird for promise management (Q vs bluebird)
    
    * Changed action button to Approve. Bonus new nav menu.
    
    * Changed action button to Approve. Bonus new nav menu.
    
    * Merging sections from #2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Header, Menu } from 'semantic-ui-react';
+
+import { fetchAllCards, fetchMySection, fetchMyCards } from '../redux/trello/actions';
 import Navigator from '../components/navigator/Navigator';
-import NavigatorItem from '../components/navigator/NavigatorItem';
 import Content from '../components/ui/Content';
+
+class App extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    handleAllCards: PropTypes.func,
+    handleSectionCards: PropTypes.func,
+    handleMyCards: PropTypes.func,
+  }
+
+  constructor() {
+    super();
+
+    this.state = {
+      show: 'all',
+    };
+    this.handleShowCards = this.handleShowCards.bind(this);
+  }
+
+  render() {
+    const { children } = this.props;
+
+    const styles = {
+      banner: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#222',
+        padding: '20px 0 0 20px',
+        width: '100%',
+      },
+      header: {
+        color: '#fff',
+      },
+    };
+
+    return (
+      <div>
+        <Navigator>
+          <div style={styles.banner}>
+            <Header as="h2" style={styles.header}>Rangle Weekly Preview</Header>
+            <Menu inverted pointing secondary>
+              <Menu.Item name="all cards" active={this.state.show === 'all'} onClick={() => this.handleShowCards('all')} />
+              <Menu.Item name="my section" active={this.state.show === 'section'} onClick={() => this.handleShowCards('section')} />
+              <Menu.Item name="my cards" active={this.state.show === 'me'} onClick={() => this.handleShowCards('me')} />
+            </Menu>
+          </div>
+        </Navigator>
+        <Content isVisible>
+          { children }
+        </Content>
+      </div>
+    );
+  }
+
+  handleShowCards(filter) {
+<<<<<<< HEAD
+    const { handleAllCards, handleSectionCards, handleMyCards } = this.props;
+
+    const actions = {
+      'all': handleAllCards,
+      'section': handleSectionCards,
+=======
+    const { handleAllCards, handleMyCards } = this.props;
+
+    const actions = {
+      'all': handleAllCards,
+>>>>>>> cf164bad06ee754d07295636dbb432c925474dd8
+      'me': handleMyCards,
+    };
+
+    this.setState({ show: filter });
+    actions[filter]();
+  }
+}
 
 function mapStateToProps(state) {
   return {
@@ -10,25 +87,13 @@ function mapStateToProps(state) {
   };
 }
 
-function mapDispatchToProps() {
-  return {};
-}
-
-const App = ({ children }) => {
-  return (
-    <div>
-      <Navigator>
-        <div className="flex flex-auto">
-          <NavigatorItem className="p1">
-            <span className="red bold">Rangle Weekly Preview</span>
-          </NavigatorItem>
-        </div>
-      </Navigator>
-      <Content isVisible>
-        { children }
-      </Content>
-    </div>
-  );
+const mapDispatchToProps = {
+  handleAllCards: fetchAllCards,
+<<<<<<< HEAD
+  handleSectionCards: fetchMySection,
+=======
+>>>>>>> cf164bad06ee754d07295636dbb432c925474dd8
+  handleMyCards: fetchMyCards,
 };
 
 export default connect(
