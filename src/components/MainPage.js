cf164bad06ee754d07295636dbb432c925474dd8
Roberto von Schoettler <roberto.vonschoettler@gmail.com>
2017-04-25 14:50:19 -0400

    Changed action button to Approve. Bonus new nav menu.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,21 +7,16 @@ import Preview from './Preview';
 export default class MainPage extends Component {
   static propTypes = {
     content: PropTypes.array.isRequired,
-    history: PropTypes.object.isRequired,
     route: PropTypes.object.isRequired,
-    fetchAllCards: PropTypes.func.isRequired,
-    fetchMyCards: PropTypes.func.isRequired,
   }
 
   render() {
-    const { content, fetchAllCards, fetchMyCards } = this.props;
+    const { content} = this.props;
 
     return (
       <Container>
         <Column>
           <Preview
-            fetchAllCards={fetchAllCards}
-            fetchMyCards={fetchMyCards}
             content={content}
           />
         </Column>
