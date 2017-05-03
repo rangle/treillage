88fbecbbe2f4549d66217450ffe3272eb228efe0
Roberto von Schoettler <robertovonschoettler@Robertos-MacBook-Pro.local>
2017-05-03 14:05:01 -0400

    greatly increased rules test quality. Refeactored rules to constructor

--- src/services/rules.js
+++ src/services/rules.js
@@ -1,80 +1,77 @@
 // import R from 'ramda';
 import leven from 'leven';
-import names from '../dictionary/names.json';
-
-const maxSize = 100;
-
-const errors = {
-  noEmptyBody: 'This card\'s description is empty.',
-  maxLength: `This card is over ${maxSize} characters long, please shorten.`,
-  nameCheck: `Possible mispellings.`,
-};
-
-const noEmptyBody = (card) => card.desc.length === 0 && { text: errors.noEmptyBody };
-
-const maxLength = (card) => {
-  const size = (card.name + ' ' + card.desc).split(' ').length;
-
-  return size > maxSize && { text: errors.maxLength };
-};
-
-const nameCheck = (card) => {
-  const levenTreshold = 5; // The treshold on how distant the word has to be to be suggested as a name
-
-  if (card.desc.length > 0) {
-    const possibleNames = card.desc.match(new RegExp(/([A-Z][\w]*(\s+(van|von|de|da|Van))?(\s+[A-Z][\w]*))/g));
-
-    if (possibleNames) {
-      // Comments as reference:
-
-      // Ideally, processing Slack/Resource Guru happens at the start of the app.
-      // The below people output has been cached as names.list
-
-      // const filterByRg = p => names.rg.indexOf(p.realName) !== -1;
-      // const filterByName = p => p.realName;
-      // const filterByEmail = p => p.email && p.email.split('@')[1] === 'rangle.io';
-      // const removeEmails =  p => ({ name: p.realName, username: p.name });
-      // const removeUsernames = p => new RegExp(p.name.replace(' ', '|'), 'i').test(p.username) ? p.name : ([p.name, p.username]);
-      //
-      // const people = R.flatten(names.slack
-      //   // .filter(filterByRg)
-      //   .filter(filterByName)
-      //   .filter(filterByEmail)
-      //   .map(removeEmails)
-      //   .map(removeUsernames)
-      // );
-
-      const suggest = (suggestions) => {
-        return suggestions.reduce((suggestion, { name, closeTo }) => {
-          return `${suggestion} \n ${name} - Perhaps you meant: ${closeTo.join(', ')}`;
-        }, '');
-      };
-
-      const message = suggest(
-        possibleNames
-          .filter(possibleName => names.list.indexOf(possibleName) === -1)
-          .map(possibleName => ({
-            name: possibleName,
-            closeTo: names.list.filter(name => leven(possibleName, name) < levenTreshold),
-          }))
-          .filter(possibleMatch => possibleMatch.closeTo.length > 0)
-      );
-
-      return message && { text: `${errors.nameCheck} \n ${message}`, options: ['ignorable'] };
-    }
-  }
-};
+import defaultNames from '../dictionary/names.json';
 
 export const PASS = 'Checks passed.';
 
-export {
-  noEmptyBody,
-  maxLength,
-  nameCheck,
-};
+export default function Rules({ maxSize = 100, names = defaultNames.list }) {
+  this.maxSize = maxSize;
+  this.names = names;
 
-export default [
-  noEmptyBody,
-  maxLength,
-  nameCheck,
-];
+  this.errors = {
+    noEmptyBody: 'This card\'s description is empty.',
+    maxLength: `This card is over ${this.maxSize} characters long, please shorten.`,
+    nameCheck: (message) => `Possible mispellings. \n${message}`,
+  };
+
+  this.noEmptyBody = (card) => card.desc.length === 0 && { text: this.errors.noEmptyBody };
+
+  this.maxLength = (card) => {
+    const size = (card.name + ' ' + card.desc).split(' ').length;
+
+    return size > this.maxSize && { text: this.errors.maxLength };
+  };
+
+  this.nameCheck = (card) => {
+    const levenTreshold = 5; // The treshold on how distant the word has to be to be suggested as a name
+
+    if (card.desc.length > 0) {
+      const possibleNames = card.desc.match(new RegExp(/([A-Z][\w]*(\s+(van|von|de|da|Van))?(\s+[A-Z][\w]*))/g));
+
+      if (possibleNames) {
+        // Comments as reference:
+
+        // Ideally, processing Slack/Resource Guru happens at the start of the app.
+        // The below people output has been cached as names.list
+
+        // const filterByRg = p => names.rg.indexOf(p.realName) !== -1;
+        // const filterByName = p => p.realName;
+        // const filterByEmail = p => p.email && p.email.split('@')[1] === 'rangle.io';
+        // const removeEmails =  p => ({ name: p.realName, username: p.name });
+        // const removeUsernames = p => new RegExp(p.name.replace(' ', '|'), 'i').test(p.username) ? p.name : ([p.name, p.username]);
+        //
+        // const people = R.flatten(names.slack
+        //   // .filter(filterByRg)
+        //   .filter(filterByName)
+        //   .filter(filterByEmail)
+        //   .map(removeEmails)
+        //   .map(removeUsernames)
+        // );
+
+        const suggest = (suggestions) => {
+          return suggestions.reduce((suggestion, { name, closeTo }) => {
+            return `${suggestion && suggestion + '\n'}${name} - Perhaps you meant: ${closeTo.join(', ')}`;
+          }, '');
+        };
+
+        const message = suggest(
+          possibleNames
+            .filter(possibleName => this.names.indexOf(possibleName) === -1)
+            .map(possibleName => ({
+              name: possibleName,
+              closeTo: this.names.filter(name => leven(possibleName, name) < levenTreshold),
+            }))
+            .filter(possibleMatch => possibleMatch.closeTo.length > 0)
+        );
+
+        return message && { text: this.errors.nameCheck(message), options: ['ignorable'] };
+      }
+    }
+  };
+
+  this.list = [
+    this.noEmptyBody,
+    this.maxLength,
+    this.nameCheck,
+  ];
+}
--- src/services/rules.test.js
+++ src/services/rules.test.js
@@ -1,80 +1,63 @@
 import test from 'ava';
-import rules, { noEmptyBody, maxLength, nameCheck } from './rules';
+import Rules from './rules';
 
-const emptyBodyCard = {
+const card = {
   id: '5901b96834bd455486a22657',
-  desc: '',
-  descData: {
-    'emoji': {},
-  },
-};
-
-const longCard = {
-  id: '5901b96834bd455486a22657',
-  desc: `Contrary to popular belief, Lorem Ipsum is not simply random text.
-  It has roots in a piece of classical Latin literature from 45 BC, making
-  it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
-  College in Virginia, looked up one of the more obscure Latin words, consectetur,
-  from a Lorem Ipsum passage, and going through the cites of the word in classical
-  literature, discovered the undoubtable source. Lorem Ipsum comes from sections
-  1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil)
-  by Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
-   very popular during the Renaissance. The first line of Lorem Ipsum,
-  "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-  descData: {
-    'emoji': {},
-  },
-};
-
-const mispelledCard = {
-  id: '5901b96834bd455486a22657',
-  desc: `[[Mickel Besco]] has decided to pursue opportunities outside of Rangle.
-    Throughout his time here Miguel worked on many projects, Podium,
-    Shape Security and Veracode to name just a few, and contributed to Rangle in other ways,
-    like the blog. It was a pleasure to see Miguel grow as a professional,
-    and become proficient in both AngularJS, Angular and React.`,
-  descData: {
-    'emoji': {},
-  },
-};
-
-const validCard = {
-  id: '5901b96834bd455486a22657',
-  desc: `[[Miguel Vesco]] has decided to pursue opportunities outside of Rangle.
-    Throughout his time here Miguel worked on many projects, Podium,
-    Shape Security and Veracode to name just a few, and contributed to Rangle in other ways,
-    like the blog. It was a pleasure to see Miguel grow as a professional,
-    and become proficient in both AngularJS, Angular and React.`,
+  desc: `Richard McClintock, a Latin professor at Hampden-Sydney
+    College in Virginia, looked up one of the more obscure Latin words, consectetur,
+    from a Lorem Ipsum passage, and going through the cites of the word in classical
+    literature, discovered the undoubtable source.`,
   descData: {
     'emoji': {},
   },
 };
 
 test('No empty body rule', t => {
+  const { noEmptyBody, errors } = new Rules({});
+  const emptyBodyCard = {
+    ...card,
+    desc: '',
+  };
+
   const error = noEmptyBody(emptyBodyCard);
 
-  t.truthy(error);
+  t.is(error.text, errors.noEmptyBody);
 });
 
 test('No long card rule',  t => {
+  const { maxLength, errors } = new Rules({ maxSize: 70 });
+  const longCard = {
+    ...card,
+    desc: card.desc + card.desc,
+  };
+
   const error = maxLength(longCard);
 
-  t.truthy(error);
+  t.is(error.text, errors.maxLength);
 });
 
 test('No mispelled names rule', t => {
+  const mispelledName = 'Rickard McClinton';
+  const correctName = 'Richard McClintock';
+  const otherNames = ['Ricardo Maquinto', 'Richard Lionheart'];
+  const { nameCheck, errors } = new Rules({ names: [correctName, ...otherNames] });
+  const mispelledCard = {
+    ...card,
+    desc: card.desc.replace(correctName, mispelledName),
+  };
+
   const error =  nameCheck(mispelledCard);
 
-  t.truthy(error);
+  const message = `${mispelledName} - Perhaps you meant: ${correctName}`;
+  t.is(error.text, errors.nameCheck(message));
 });
 
 test('Valid card should pass all rules', t => {
-  t.plan(rules.length);
+  const { list } = new Rules({});
+  t.plan(list.length);
 
-  rules.forEach(rule => {
-    const error = rule(validCard);
+  list.forEach(rule => {
+    const error = rule(card);
 
     t.falsy(error);
   });
